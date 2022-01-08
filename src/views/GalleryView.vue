b8c4c317ec61505e73397fb7aefcd0a60b8de633
KubaM-97 <jakubm16@interia.pl>
2022-01-08 14:07:59 +0100

    feat() - add prevent condition if no photos found for this date

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,15 +1,32 @@
 <template>
-    <GalleryImages />
+    <div class='text-center mt-5'>
+        <p v-if="!photos.length">No photos found for this date</p>
+        <template v-else>
+            <GalleryImages :photos="photos" />
+        </template>
+    </div>
 </template>
 
 <script>
 import GalleryImages from '@/components/gallery/GalleryImages';
+
+import { computed } from 'vue';
+import { useStore } from "vuex";
 
 export default {
     name: 'GalleryView',
     components: {
         GalleryImages,
     },
+    setup() {
+
+        const store = useStore();
+        const photos = computed(() => store.state.photos);
+
+        return {
+            photos
+        }
+    }
 }
 </script>
 
