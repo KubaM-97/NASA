28a9e31e41931ce7d731571cc23205736081bde1
KubaM-97 <jakubm16@interia.pl>
2022-01-08 12:57:42 +0100

    feat(config - selectDate) - add some basic v-model and max logic for formattedDate YYYY-MM-DD

--- src/components/config/SelectDate.vue
+++ src/components/config/SelectDate.vue
@@ -1,13 +1,40 @@
 <template>
   <div class='mx-2'>
     <label for='selectDate' class='mb-2 d-block'>Select date:</label>
-    <input id='selectDate' type='date' class='d-block pl-2' />
+    <input id='selectDate' type='date'  v-model="date" :max='date' class='d-block pl-2' />
   </div>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue'
+
 export default {
   name: 'SelectDate',
+  setup(){
+
+    const today = ref(new Date())
+    const date = ref('')
+
+    const formatDate = () => {
+
+      const year = today.value.getFullYear()
+      const month = today.value.getMonth() + 1
+      const day = today.value.getDate()
+      
+      const formattedMonth = month < 10 ? `0${month}` : month;
+      const formattedDay = month < 10 ? `0${day}` : day;
+
+      date.value = `${year}-${formattedMonth}-${formattedDay}` 
+    }
+    
+    onMounted(()=>{
+      formatDate()
+    })
+
+    return {
+      date
+    }
+  }
 }
 </script>
 
