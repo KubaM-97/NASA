d11771df5959e01637281a8eb357218f742e5d1a
KubaM-97 <jakubm16@interia.pl>
2022-01-08 14:06:05 +0100

    feat() - successfully fetch photos from API

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
     state: {
         numberOfImages: 10,
         selectedDate: null,
+        photos: [],
+        apiKey: '5kUQpbi1gtlKlgdnJpKjlQTcjIu6sak1cmnvtSfs'
     },
     getters: {
         
     },
     mutations: {
+        setPhotos(state, photos) {
+            state.photos = photos
+        },
         setNumberOfImages(state, numberOfImages) {
             state.numberOfImages = numberOfImages
         },
@@ -17,8 +23,14 @@ export default createStore({
         }
     },
     actions: {
-        fetchPhotos( { state } ) {
-            console.log(state.numberOfImages, state.selectedDate);
+        fetchPhotos( { state, commit } ) {
+            const { selectedDate, apiKey } = state;
+            axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${selectedDate}&api_key=${apiKey}`,
+            )
+            .then(response => {
+                commit("setPhotos", response.data.photos)
+            })
+            
         }
     },
 })
