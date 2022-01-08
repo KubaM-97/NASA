b06342f6fc58335a8b4f399a36882684e29c0d74
KubaM-97 <jakubm16@interia.pl>
2022-01-08 12:52:52 +0100

    feat(config - numberOfImages) - add some basic v-model logic

--- src/components/config/NumberOfImages.vue
+++ src/components/config/NumberOfImages.vue
@@ -1,16 +1,23 @@
 <template>
   <div class='mx-2'>
     <label for='numberOfImages' class='mb-2 d-block'>Number of images:</label>
-    <input id='numberOfImages' type='number' class='d-block pl-2'/>
+    <input id='numberOfImages' type='number' v-model.number='numberOfImages' class='d-block pl-2' />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'NumberOfImages',
+  setup(){
+    
+    const numberOfImages = ref(0);
+
+    return {
+      numberOfImages
+    }
+
+  }
 }
 </script>
-
-<style>
-
-</style>
