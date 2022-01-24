61d9b816e3967445001aa6ebd73d9b0000a902ea
KubaM-97 <jakubm16@interia.pl>
2022-01-24 23:09:13 +0100

    fix(config) - fix max attribute in input date

--- src/components/config/SelectDate.vue
+++ src/components/config/SelectDate.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='form-group mx-md-2 my-3 col-md-4'>
     <label for='selectDate' class='mb-md-2 d-block'>Select date:</label>
-    <input id='selectDate' type='date' v-model="date" :max='date' class='form-control d-block pl-2' />
+    <input id='selectDate' type='date' v-model="date" :max="today" class='form-control d-block pl-2' />
   </div>
 </template>
 
@@ -17,20 +17,21 @@ export default {
     const today = ref(new Date())
     const date = ref('')
 
-    const formatDate = () => {
+    const formatDate = (date) => {
 
-      const year = today.value.getFullYear()
-      const month = today.value.getMonth() + 1
-      const day = today.value.getDate()
+      const year = date.value.getFullYear()
+      const month = date.value.getMonth() + 1
+      const day = date.value.getDate()
       
       const formattedMonth = month < 10 ? `0${month}` : month;
-      const formattedDay = month < 10 ? `0${day}` : day;
+      const formattedDay = day < 10 ? `0${day}` : day;
 
-      date.value = `${year}-${formattedMonth}-${formattedDay}` 
+      return `${year}-${formattedMonth}-${formattedDay}` 
     }
     
     onMounted(()=>{
-      formatDate()
+      date.value = formatDate(today)
+      today.value = formatDate(today)
     })
 
     watch(date, (val) => {
@@ -38,7 +39,8 @@ export default {
     })
 
     return {
-      date
+      today,
+      date,
     }
   }
 }
